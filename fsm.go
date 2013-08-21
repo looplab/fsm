18a612154c255c44da19ede94f1254b25b73e517
Max Persson <max@maxpersson.com>
2013-08-21 17:25:26 +0200

    Add main source and tests

---
+++ fsm.go
@@ -0,0 +1,359 @@
+// Copyright 2013 Loop Lab
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package fms implements a finite state machine
+//
+// It is heavily based on two FSM implementations:
+//
+// Javascript Finite State Machine
+// https://github.com/jakesgordon/javascript-state-machine
+//
+// Fysom for Python
+// https://github.com/oxplot/fysom (forked at https://github.com/mriehl/fysom)
+//
+package fsm
+
+import (
+	"fmt"
+	"strings"
+)
+
+// FSM is the state machine and has to be created with NewFSM
+type FSM struct {
+	// current is the state that the FSM is currently in
+	current string
+
+	// transitions maps events and source states to destination states.
+	transitions map[eKey]string
+
+	// callbacks maps events and targers to callback functions.
+	callbacks map[cKey]Callback
+
+	// transition is the internal transition functions used either directly
+	// or when Transition is called in an asynchronous state transition.
+	transition func()
+}
+
+// Event is the info that get passed as a reference in the callbacks
+// It contins the FSM calling it, event name, source and destination states.
+type Event struct {
+	FSM      *FSM
+	Event    string
+	Src      string
+	Dst      string
+	canceled bool
+	async    bool
+}
+
+// Cancel can be called in before_<EVENT> or leave_<STATE> to cancel the
+// current transition before it happens.
+func (e *Event) Cancel() {
+	e.canceled = true
+}
+
+// Async can be called in leave_<STATE> to do an asynchronous state transition
+// The current state transition will be on hold in the old state until a final
+// call to Transition is made. This will comlete the transition and possibly
+// call the other callbacks.
+func (e *Event) Async() {
+	e.async = true
+}
+
+// Events is a shorthand for defining the transition map in NewFSM
+type Events []EventDesc
+
+// EventDesc represents an event in the FSM
+//
+// The event can have one or more source states that is valid for performing
+// the transition. If the FSM is in one of the source states it will end up in
+// the specified destination state.
+type EventDesc struct {
+	// Name is the event name used when calling for a transition
+	Name string
+
+	// Src is a slice of source states that the FSM must be in to perform a
+	// state transition.
+	Src []string
+
+	// Dst is the destination state that the FSM will be in if the transition
+	// succeds.
+	Dst string
+}
+
+// Callbacks is a shorthand for defining the callbacks in NewFSM
+type Callbacks map[string]Callback
+
+// Callback is a function type that callbacks should use. Event is the current event info as the callback happens.
+type Callback func(*Event)
+
+// Type of callback used in the internal mapping to functions
+type callbackType int
+
+const (
+	noCallback callbackType = iota
+	beforeEvent
+	leaveState
+	enterState
+	afterEvent
+)
+
+// cKey is a struct key used for keeping the callbacks mapped to a target
+type cKey struct {
+	// target is either the name of a state or an event depending on which
+	// callback type the key refers to. It can also be "" for a non-targeted
+	// callback like before_event
+	target string
+
+	// callbackType is the situation when the callback will be run
+	callbackType callbackType
+}
+
+// eKey is a struct key used for storing the transition map
+type eKey struct {
+	// event is the name of the event that the keys refers to
+	event string
+
+	// src is the source from where the event can transition
+	src string
+}
+
+// NewFSM constructs a FSM from events and callbacks
+//
+// The events and transitions are specified as a slice of Event structs
+// specified as Events. Each Event is mapped to one or more internal
+// transitions from Event.Src to Event.Dst.
+//
+// Callbacks are added as a map specified as Callbacks where the key is parsed
+// as the callback event as follows, and called in the same order:
+//
+// 1. before_<EVENT> - called before event named <EVENT>
+//
+// 2. before_event - called before all events
+//
+// 3. leave_<OLD_STATE> - called before leaving <OLD_STATE>
+//
+// 4. leave_state - called before leaving all states
+//
+// 5. enter_<NEW_STATE> - called after eftering <NEW_STATE>
+//
+// 6. enter_state - called after entering all states
+//
+// 7. after_<EVENT> - called after event named <EVENT>
+//
+// 8. after_event - called after all events
+//
+// There are also two short form versions for the most commonly used callbacks:
+//
+// <NEW_STATE> - called after entering <NEW_STATE>
+//
+// <EVENT> - called after event named <EVENT>
+func NewFSM(initial string, events Events, callbacks Callbacks) *FSM {
+	var f FSM
+	f.current = initial
+	f.transitions = make(map[eKey]string)
+	f.callbacks = make(map[cKey]Callback)
+
+	// Build transition map and store sets of all events and states
+	allEvents := make(map[string]bool)
+	allStates := make(map[string]bool)
+	for _, e := range events {
+		for _, src := range e.Src {
+			f.transitions[eKey{e.Name, src}] = e.Dst
+			allStates[src] = true
+			allStates[e.Dst] = true
+		}
+		allEvents[e.Name] = true
+	}
+
+	// Map all callbacks to events/states
+	for name, c := range callbacks {
+		var target string
+		var cType callbackType
+
+		switch {
+		case strings.HasPrefix(name, "before_"):
+			target = strings.TrimPrefix(name, "before_")
+			if target == "event" {
+				target = ""
+				cType = beforeEvent
+			} else if _, ok := allEvents[target]; ok {
+				cType = beforeEvent
+			}
+		case strings.HasPrefix(name, "leave_"):
+			target = strings.TrimPrefix(name, "leave_")
+			if target == "state" {
+				target = ""
+				cType = leaveState
+			} else if _, ok := allStates[target]; ok {
+				cType = leaveState
+			}
+		case strings.HasPrefix(name, "enter_"):
+			target = strings.TrimPrefix(name, "enter_")
+			if target == "state" {
+				target = ""
+				cType = enterState
+			} else if _, ok := allStates[target]; ok {
+				cType = enterState
+			}
+		case strings.HasPrefix(name, "after_"):
+			target = strings.TrimPrefix(name, "after_")
+			if target == "event" {
+				target = ""
+				cType = afterEvent
+			} else if _, ok := allEvents[target]; ok {
+				cType = afterEvent
+			}
+		default:
+			target = name
+			if _, ok := allStates[target]; ok {
+				cType = enterState
+			} else if _, ok := allEvents[target]; ok {
+				cType = afterEvent
+			}
+		}
+
+		if cType != noCallback {
+			f.callbacks[cKey{target, cType}] = c
+		}
+	}
+
+	return &f
+}
+
+// Current returns the current state of the FSM.
+func (f *FSM) Current() string {
+	return f.current
+}
+
+// Is returns true if state is the current state.
+func (f *FSM) Is(state string) bool {
+	return state == f.current
+}
+
+// Can returns true if event can occur in the current state.
+func (f *FSM) Can(event string) bool {
+	_, ok := f.transitions[eKey{event, f.current}]
+	return ok && (f.transition == nil)
+}
+
+// Can returns true if event can not occure in the current state.
+// It is a convenience method to help code read nicely.
+func (f *FSM) Cannot(event string) bool {
+	return !f.Can(event)
+}
+
+// Event initiates a state transition with the named event.
+//
+// It will return nil if the state change is ok or one of these errors:
+//
+// - event X inappropriate because previous transition did not complete
+//
+// - event X inappropriate in current state Y
+//
+// - transition innapropriate because no state change in progress
+//
+// The last error should never occur in this situation and is a sign of an
+// internal bug.
+func (f *FSM) Event(event string) error {
+	if f.transition != nil {
+		return fmt.Errorf("event %s inappropriate because previous transition did not complete", event)
+	}
+
+	dst, ok := f.transitions[eKey{event, f.current}]
+	if !ok {
+		return fmt.Errorf("event %s inappropriate in current state %s", event, f.current)
+	}
+
+	if f.current == dst {
+		return nil
+	}
+
+	e := &Event{f, event, f.current, dst, false, false}
+
+	// Call the before_ callbacks, first the named then the general version.
+	if fn, ok := f.callbacks[cKey{event, beforeEvent}]; ok {
+		fn(e)
+		if e.canceled {
+			return nil
+		}
+	}
+	if fn, ok := f.callbacks[cKey{"", beforeEvent}]; ok {
+		fn(e)
+		if e.canceled {
+			return nil
+		}
+	}
+
+	f.transition = func() {
+		// Do the state transition.
+		f.current = dst
+
+		// Call the enter_ callbacks, first the named then the general version.
+		if fn, ok := f.callbacks[cKey{f.current, enterState}]; ok {
+			fn(e)
+		}
+		if fn, ok := f.callbacks[cKey{"", enterState}]; ok {
+			fn(e)
+		}
+
+		// Call the after_ callbacks, first the named then the general version.
+		if fn, ok := f.callbacks[cKey{event, afterEvent}]; ok {
+			fn(e)
+		}
+		if fn, ok := f.callbacks[cKey{"", afterEvent}]; ok {
+			fn(e)
+		}
+	}
+
+	// Call the leave_ callbacks, first the named then the general version.
+	if fn, ok := f.callbacks[cKey{f.current, leaveState}]; ok {
+		fn(e)
+		if e.canceled {
+			f.transition = nil
+			return nil
+		} else if e.async {
+			return nil
+		}
+	}
+	if fn, ok := f.callbacks[cKey{"", leaveState}]; ok {
+		fn(e)
+		if e.canceled {
+			f.transition = nil
+			return nil
+		} else if e.async {
+			return nil
+		}
+	}
+
+	// Perform the rest of the transition, if not asynchronous.
+	err := f.Transition()
+	if err != nil {
+		return fmt.Errorf("internal error on state transition")
+	}
+
+	return nil
+}
+
+// Transition completes an asynchrounous state change.
+//
+// The callback for leave_<STATE> must prviously have called Async on its
+// event to have initiated an asynchronous state transition.
+func (f *FSM) Transition() error {
+	if f.transition == nil {
+		return fmt.Errorf("transition inappropriate because no state change in progress")
+	}
+	f.transition()
+	f.transition = nil
+	return nil
+}
---
+++ fsm_test.go
@@ -0,0 +1,546 @@
+// Copyright 2013 Loop Lab
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package fsm
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestSameState(t *testing.T) {
+	fsm := NewFSM(
+		"start",
+		Events{
+			{Name: "run", Src: []string{"start"}, Dst: "start"},
+		},
+		Callbacks{},
+	)
+	fsm.Event("run")
+	if fsm.Current() != "start" {
+		t.FailNow()
+	}
+}
+
+func TestInappropriateEvent(t *testing.T) {
+	fsm := NewFSM(
+		"closed",
+		Events{
+			{Name: "open", Src: []string{"closed"}, Dst: "open"},
+			{Name: "close", Src: []string{"open"}, Dst: "closed"},
+		},
+		Callbacks{},
+	)
+	err := fsm.Event("close")
+	if err.Error() != "event close inappropriate in current state closed" {
+		t.FailNow()
+	}
+}
+
+func TestInvalidEvent(t *testing.T) {
+	fsm := NewFSM(
+		"closed",
+		Events{
+			{Name: "open", Src: []string{"closed"}, Dst: "open"},
+			{Name: "close", Src: []string{"open"}, Dst: "closed"},
+		},
+		Callbacks{},
+	)
+	err := fsm.Event("lock")
+	if err.Error() != "event lock inappropriate in current state closed" {
+		t.FailNow()
+	}
+}
+
+func TestMultipleSources(t *testing.T) {
+	fsm := NewFSM(
+		"one",
+		Events{
+			{Name: "first", Src: []string{"one"}, Dst: "two"},
+			{Name: "second", Src: []string{"two"}, Dst: "three"},
+			{Name: "reset", Src: []string{"one", "two", "three"}, Dst: "one"},
+		},
+		Callbacks{},
+	)
+
+	fsm.Event("first")
+	if fsm.Current() != "two" {
+		t.FailNow()
+	}
+	fsm.Event("reset")
+	if fsm.Current() != "one" {
+		t.FailNow()
+	}
+	fsm.Event("first")
+	fsm.Event("second")
+	if fsm.Current() != "three" {
+		t.FailNow()
+	}
+	fsm.Event("reset")
+	if fsm.Current() != "one" {
+		t.FailNow()
+	}
+}
+
+func TestMultipleEvents(t *testing.T) {
+	fsm := NewFSM(
+		"start",
+		Events{
+			{Name: "first", Src: []string{"start"}, Dst: "one"},
+			{Name: "second", Src: []string{"start"}, Dst: "two"},
+			{Name: "reset", Src: []string{"one"}, Dst: "reset_one"},
+			{Name: "reset", Src: []string{"two"}, Dst: "reset_two"},
+			{Name: "reset", Src: []string{"reset_one", "reset_two"}, Dst: "start"},
+		},
+		Callbacks{},
+	)
+
+	fsm.Event("first")
+	fsm.Event("reset")
+	if fsm.Current() != "reset_one" {
+		t.FailNow()
+	}
+	fsm.Event("reset")
+	if fsm.Current() != "start" {
+		t.FailNow()
+	}
+
+	fsm.Event("second")
+	fsm.Event("reset")
+	if fsm.Current() != "reset_two" {
+		t.FailNow()
+	}
+	fsm.Event("reset")
+	if fsm.Current() != "start" {
+		t.FailNow()
+	}
+}
+
+func TestGenericCallbacks(t *testing.T) {
+	beforeEvent := false
+	leaveState := false
+	enterState := false
+	afterEvent := false
+
+	fsm := NewFSM(
+		"start",
+		Events{
+			{Name: "run", Src: []string{"start"}, Dst: "end"},
+		},
+		Callbacks{
+			"before_run": func(e *Event) {
+				beforeEvent = true
+			},
+			"leave_start": func(e *Event) {
+				leaveState = true
+			},
+			"enter_end": func(e *Event) {
+				enterState = true
+			},
+			"after_run": func(e *Event) {
+				afterEvent = true
+			},
+		},
+	)
+
+	fsm.Event("run")
+	if !(beforeEvent && leaveState && enterState && afterEvent) {
+		t.FailNow()
+	}
+}
+
+func TestSpecificCallbacks(t *testing.T) {
+	beforeEvent := false
+	leaveState := false
+	enterState := false
+	afterEvent := false
+
+	fsm := NewFSM(
+		"start",
+		Events{
+			{Name: "run", Src: []string{"start"}, Dst: "end"},
+		},
+		Callbacks{
+			"before_run": func(e *Event) {
+				beforeEvent = true
+			},
+			"leave_start": func(e *Event) {
+				leaveState = true
+			},
+			"enter_end": func(e *Event) {
+				enterState = true
+			},
+			"after_run": func(e *Event) {
+				afterEvent = true
+			},
+		},
+	)
+
+	fsm.Event("run")
+	if !(beforeEvent && leaveState && enterState && afterEvent) {
+		t.FailNow()
+	}
+}
+
+func TestSpecificCallbacksShortform(t *testing.T) {
+	enterState := false
+	afterEvent := false
+
+	fsm := NewFSM(
+		"start",
+		Events{
+			{Name: "run", Src: []string{"start"}, Dst: "end"},
+		},
+		Callbacks{
+			"end": func(e *Event) {
+				enterState = true
+			},
+			"run": func(e *Event) {
+				afterEvent = true
+			},
+		},
+	)
+
+	fsm.Event("run")
+	if !(enterState && afterEvent) {
+		t.FailNow()
+	}
+}
+
+func TestCancelBeforeGenericEvent(t *testing.T) {
+	fsm := NewFSM(
+		"start",
+		Events{
+			{Name: "run", Src: []string{"start"}, Dst: "end"},
+		},
+		Callbacks{
+			"before_event": func(e *Event) {
+				e.Cancel()
+			},
+		},
+	)
+	fsm.Event("run")
+	if fsm.Current() != "start" {
+		t.FailNow()
+	}
+}
+
+func TestCancelBeforeSpecificEvent(t *testing.T) {
+	fsm := NewFSM(
+		"start",
+		Events{
+			{Name: "run", Src: []string{"start"}, Dst: "end"},
+		},
+		Callbacks{
+			"before_run": func(e *Event) {
+				e.Cancel()
+			},
+		},
+	)
+	fsm.Event("run")
+	if fsm.Current() != "start" {
+		t.FailNow()
+	}
+}
+
+func TestCancelLeaveGenericState(t *testing.T) {
+	fsm := NewFSM(
+		"start",
+		Events{
+			{Name: "run", Src: []string{"start"}, Dst: "end"},
+		},
+		Callbacks{
+			"leave_state": func(e *Event) {
+				e.Cancel()
+			},
+		},
+	)
+	fsm.Event("run")
+	if fsm.Current() != "start" {
+		t.FailNow()
+	}
+}
+
+func TestCancelLeaveSpecificState(t *testing.T) {
+	fsm := NewFSM(
+		"start",
+		Events{
+			{Name: "run", Src: []string{"start"}, Dst: "end"},
+		},
+		Callbacks{
+			"leave_start": func(e *Event) {
+				e.Cancel()
+			},
+		},
+	)
+	fsm.Event("run")
+	if fsm.Current() != "start" {
+		t.FailNow()
+	}
+}
+
+func TestAsyncTransitionGenericState(t *testing.T) {
+	fsm := NewFSM(
+		"start",
+		Events{
+			{Name: "run", Src: []string{"start"}, Dst: "end"},
+		},
+		Callbacks{
+			"leave_state": func(e *Event) {
+				e.Async()
+			},
+		},
+	)
+	fsm.Event("run")
+	if fsm.Current() != "start" {
+		t.FailNow()
+	}
+	fsm.Transition()
+	if fsm.Current() != "end" {
+		t.FailNow()
+	}
+}
+
+func TestAsyncTransitionSpecificState(t *testing.T) {
+	fsm := NewFSM(
+		"start",
+		Events{
+			{Name: "run", Src: []string{"start"}, Dst: "end"},
+		},
+		Callbacks{
+			"leave_start": func(e *Event) {
+				e.Async()
+			},
+		},
+	)
+	fsm.Event("run")
+	if fsm.Current() != "start" {
+		t.FailNow()
+	}
+	fsm.Transition()
+	if fsm.Current() != "end" {
+		t.FailNow()
+	}
+}
+
+func TestAsyncTransitionInProgress(t *testing.T) {
+	fsm := NewFSM(
+		"start",
+		Events{
+			{Name: "run", Src: []string{"start"}, Dst: "end"},
+			{Name: "reset", Src: []string{"end"}, Dst: "start"},
+		},
+		Callbacks{
+			"leave_start": func(e *Event) {
+				e.Async()
+			},
+		},
+	)
+	fsm.Event("run")
+	err := fsm.Event("reset")
+	if err.Error() != "event reset inappropriate because previous transition did not complete" {
+		t.FailNow()
+	}
+	fsm.Transition()
+	fsm.Event("reset")
+	if fsm.Current() != "start" {
+		t.FailNow()
+	}
+}
+
+func TestAsyncTransitionNotInProgress(t *testing.T) {
+	fsm := NewFSM(
+		"start",
+		Events{
+			{Name: "run", Src: []string{"start"}, Dst: "end"},
+			{Name: "reset", Src: []string{"end"}, Dst: "start"},
+		},
+		Callbacks{},
+	)
+	err := fsm.Transition()
+	if err.Error() != "transition inappropriate because no state change in progress" {
+		t.FailNow()
+	}
+}
+
+func ExampleNewFSM() {
+	fsm := NewFSM(
+		"green",
+		Events{
+			{Name: "warn", Src: []string{"green"}, Dst: "yellow"},
+			{Name: "panic", Src: []string{"yellow"}, Dst: "red"},
+			{Name: "panic", Src: []string{"green"}, Dst: "red"},
+			{Name: "calm", Src: []string{"red"}, Dst: "yellow"},
+			{Name: "clear", Src: []string{"yellow"}, Dst: "green"},
+		},
+		Callbacks{
+			"before_warn": func(e *Event) {
+				fmt.Println("before_warn")
+			},
+			"before_event": func(e *Event) {
+				fmt.Println("before_event")
+			},
+			"leave_green": func(e *Event) {
+				fmt.Println("leave_green")
+			},
+			"leave_state": func(e *Event) {
+				fmt.Println("leave_state")
+			},
+			"enter_yellow": func(e *Event) {
+				fmt.Println("enter_yellow")
+			},
+			"enter_state": func(e *Event) {
+				fmt.Println("enter_state")
+			},
+			"after_warn": func(e *Event) {
+				fmt.Println("after_warn")
+			},
+			"after_event": func(e *Event) {
+				fmt.Println("after_event")
+			},
+		},
+	)
+	fmt.Println(fsm.Current())
+	err := fsm.Event("warn")
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(fsm.Current())
+	// Output:
+	// green
+	// before_warn
+	// before_event
+	// leave_green
+	// leave_state
+	// enter_yellow
+	// enter_state
+	// after_warn
+	// after_event
+	// yellow
+}
+
+func ExampleFSM_Current() {
+	fsm := NewFSM(
+		"closed",
+		Events{
+			{Name: "open", Src: []string{"closed"}, Dst: "open"},
+			{Name: "close", Src: []string{"open"}, Dst: "closed"},
+		},
+		Callbacks{},
+	)
+	fmt.Println(fsm.Current())
+	// Output: closed
+}
+
+func ExampleFSM_Is() {
+	fsm := NewFSM(
+		"closed",
+		Events{
+			{Name: "open", Src: []string{"closed"}, Dst: "open"},
+			{Name: "close", Src: []string{"open"}, Dst: "closed"},
+		},
+		Callbacks{},
+	)
+	fmt.Println(fsm.Is("closed"))
+	fmt.Println(fsm.Is("open"))
+	// Output:
+	// true
+	// false
+}
+
+func ExampleFSM_Can() {
+	fsm := NewFSM(
+		"closed",
+		Events{
+			{Name: "open", Src: []string{"closed"}, Dst: "open"},
+			{Name: "close", Src: []string{"open"}, Dst: "closed"},
+		},
+		Callbacks{},
+	)
+	fmt.Println(fsm.Can("open"))
+	fmt.Println(fsm.Can("close"))
+	// Output:
+	// true
+	// false
+}
+
+func ExampleFSM_Cannot() {
+	fsm := NewFSM(
+		"closed",
+		Events{
+			{Name: "open", Src: []string{"closed"}, Dst: "open"},
+			{Name: "close", Src: []string{"open"}, Dst: "closed"},
+		},
+		Callbacks{},
+	)
+	fmt.Println(fsm.Cannot("open"))
+	fmt.Println(fsm.Cannot("close"))
+	// Output:
+	// false
+	// true
+}
+
+func ExampleFSM_Event() {
+	fsm := NewFSM(
+		"closed",
+		Events{
+			{Name: "open", Src: []string{"closed"}, Dst: "open"},
+			{Name: "close", Src: []string{"open"}, Dst: "closed"},
+		},
+		Callbacks{},
+	)
+	fmt.Println(fsm.Current())
+	err := fsm.Event("open")
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(fsm.Current())
+	err = fsm.Event("close")
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(fsm.Current())
+	// Output:
+	// closed
+	// open
+	// closed
+}
+
+func ExampleFSM_Transition() {
+	fsm := NewFSM(
+		"closed",
+		Events{
+			{Name: "open", Src: []string{"closed"}, Dst: "open"},
+			{Name: "close", Src: []string{"open"}, Dst: "closed"},
+		},
+		Callbacks{
+			"leave_closed": func(e *Event) {
+				e.Async()
+			},
+		},
+	)
+	err := fsm.Event("open")
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(fsm.Current())
+	err = fsm.Transition()
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(fsm.Current())
+	// Output:
+	// closed
+	// open
+}
