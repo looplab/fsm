36e02246181e44177d157e7b53fd40a8d9d948cf
Juan B. Rodriguez <jbrodriguez@apertoire.net>
2014-09-04 20:29:31 -0500

    fire after events even if source state is equal to destination state

--- fsm.go
+++ fsm.go
@@ -323,6 +323,18 @@ func (f *FSM) Event(event string, args ...interface{}) error {
 	}
 
 	if f.current == dst {
+
+		// Call the after events even if destination state is the same as the current state
+		e := &Event{f, event, f.current, dst, nil, args, false, false}
+
+		// Call the after_ callbacks, first the named then the general version.
+		if fn, ok := f.callbacks[cKey{event, afterEvent}]; ok {
+			fn(e)
+		}
+		if fn, ok := f.callbacks[cKey{"", afterEvent}]; ok {
+			fn(e)
+		}
+
 		return nil
 	}
 
