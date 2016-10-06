4683fd0f7b0036b125959ec4017d1a24c901ce07
Kristoffer A. Iversen <kristoffer.a.iversen@gmail.com>
2016-10-06 17:58:48 +0200

    Added test to confirm a thread safety issue. Test fails when run with -race

--- fsm_test.go
+++ fsm_test.go
@@ -16,6 +16,7 @@ package fsm
 
 import (
 	"fmt"
+	"sync"
 	"testing"
 )
 
@@ -507,6 +508,27 @@ func TestCallbackArgs(t *testing.T) {
 		},
 	)
 	fsm.Event("run", "test")
+}
+
+func TestThreadSafetyRaceCondition(t *testing.T) {
+	fsm := NewFSM(
+		"start",
+		Events{
+			{Name: "run", Src: []string{"start"}, Dst: "end"},
+		},
+		Callbacks{
+			"run": func(e *Event) {
+			},
+		},
+	)
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		_ = fsm.Current()
+	}()
+	fsm.Event("run")
+	wg.Wait()
 }
 
 func ExampleNewFSM() {
