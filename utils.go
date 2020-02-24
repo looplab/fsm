9ea0a8d1689d25ca6ca0bd78b3d1ab979b1aef40
Manuel Vogel <mavogel@posteo.de>
2020-02-24 13:11:06 +0100

    feat: adds mermaid graph output (#50)
    * chore: ignores .coverprofile
    
    * feat: adds mermaid visualize output
    
    * fix: sorts eKeys to have a reproducible graph output
    
    * fix: uses non current transitions with sort as well
    
    * test: adds third transition to verify sorting
    
    * fix: replaces ReplaceAll with Replace due to go1.11
    
    * fix: uses ReplaceAll in tests instead of Replace

--- utils.go
+++ utils.go
@@ -3,7 +3,56 @@ package fsm
 import (
 	"bytes"
 	"fmt"
+	"sort"
 )
+
+// VisualizeType the type of the visualization
+type VisualizeType string
+
+const (
+	// GRAPHVIZ the type for graphviz output (http://www.webgraphviz.com/)
+	GRAPHVIZ VisualizeType = "graphviz"
+	// MERMAID the type for mermaid output (https://mermaid-js.github.io/mermaid-live-editor/)
+	MERMAID VisualizeType = "mermaid"
+)
+
+// VisualizeWithType outputs a visualization of a FSM in the desired format.
+// If the type is not given it defaults to GRAPHVIZ
+func VisualizeWithType(fsm *FSM, visualizeType VisualizeType) (string, error) {
+	switch visualizeType {
+	case GRAPHVIZ:
+		return Visualize(fsm), nil
+	case MERMAID:
+		return visualizeForMermaid(fsm), nil
+	default:
+		return "", fmt.Errorf("unknown VisualizeType: %s", visualizeType)
+	}
+}
+
+// visualizeForMermaid outputs a visualization of a FSM in Mermaid format.
+func visualizeForMermaid(fsm *FSM) string {
+	var buf bytes.Buffer
+
+	// we sort the key alphabetically to have a reproducible graph output
+	sortedEKeys := make([]eKey, 0)
+	for k := range fsm.transitions {
+		sortedEKeys = append(sortedEKeys, k)
+	}
+	sort.Slice(sortedEKeys, func(i, j int) bool {
+		return sortedEKeys[i].src < sortedEKeys[j].src
+	})
+
+	buf.WriteString(fmt.Sprintf(`graph fsm`))
+	buf.WriteString("\n")
+
+	for _, k := range sortedEKeys {
+		v := fsm.transitions[k]
+		buf.WriteString(fmt.Sprintf(`    %s -->|%s| %s`, k.src, k.event, v))
+		buf.WriteString("\n")
+	}
+
+	return buf.String()
+}
 
 // Visualize outputs a visualization of a FSM in Graphviz format.
 func Visualize(fsm *FSM) string {
@@ -11,11 +60,21 @@ func Visualize(fsm *FSM) string {
 
 	states := make(map[string]int)
 
+	// we sort the key alphabetically to have a reproducible graph output
+	sortedEKeys := make([]eKey, 0)
+	for k := range fsm.transitions {
+		sortedEKeys = append(sortedEKeys, k)
+	}
+	sort.Slice(sortedEKeys, func(i, j int) bool {
+		return sortedEKeys[i].src < sortedEKeys[j].src
+	})
+
 	buf.WriteString(fmt.Sprintf(`digraph fsm {`))
 	buf.WriteString("\n")
 
 	// make sure the initial state is at top
-	for k, v := range fsm.transitions {
+	for _, k := range sortedEKeys {
+		v := fsm.transitions[k]
 		if k.src == fsm.current {
 			states[k.src]++
 			states[v]++
@@ -24,7 +83,8 @@ func Visualize(fsm *FSM) string {
 		}
 	}
 
-	for k, v := range fsm.transitions {
+	for _, k := range sortedEKeys {
+		v := fsm.transitions[k]
 		if k.src != fsm.current {
 			states[k.src]++
 			states[v]++
@@ -35,7 +95,15 @@ func Visualize(fsm *FSM) string {
 
 	buf.WriteString("\n")
 
+	sortedStateKeys := make([]string, 0)
 	for k := range states {
+		sortedStateKeys = append(sortedStateKeys, k)
+	}
+	sort.Slice(sortedStateKeys, func(i, j int) bool {
+		return sortedStateKeys[i] < sortedStateKeys[j]
+	})
+
+	for _, k := range sortedStateKeys {
 		buf.WriteString(fmt.Sprintf(`    "%s";`, k))
 		buf.WriteString("\n")
 	}
---
+++ utils_test.go
@@ -0,0 +1,68 @@
+package fsm
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+)
+
+func TestGraphvizOutput(t *testing.T) {
+	fsmUnderTest := NewFSM(
+		"closed",
+		Events{
+			{Name: "open", Src: []string{"closed"}, Dst: "open"},
+			{Name: "close", Src: []string{"open"}, Dst: "closed"},
+			{Name: "part-close", Src: []string{"intermediate"}, Dst: "closed"},
+		},
+		Callbacks{},
+	)
+
+	got := Visualize(fsmUnderTest)
+	wanted := `
+digraph fsm {
+    "closed" -> "open" [ label = "open" ];
+    "intermediate" -> "closed" [ label = "part-close" ];
+    "open" -> "closed" [ label = "close" ];
+
+    "closed";
+    "intermediate";
+    "open";
+}`
+	normalizedGot := strings.Replace(got, "\n", "", -1)
+	normalizedWanted := strings.ReplaceAll(wanted, "\n", "")
+	if normalizedGot != normalizedWanted {
+		t.Errorf("build graphivz graph failed. \nwanted \n%s\nand got \n%s\n", wanted, got)
+		fmt.Println([]byte(normalizedGot))
+		fmt.Println([]byte(normalizedWanted))
+	}
+}
+
+func TestMermaidOutput(t *testing.T) {
+	fsmUnderTest := NewFSM(
+		"closed",
+		Events{
+			{Name: "open", Src: []string{"closed"}, Dst: "open"},
+			{Name: "close", Src: []string{"open"}, Dst: "closed"},
+			{Name: "part-close", Src: []string{"intermediate"}, Dst: "closed"},
+		},
+		Callbacks{},
+	)
+
+	got, err := VisualizeWithType(fsmUnderTest, MERMAID)
+	if err != nil {
+		t.Errorf("got error for visualizing with type MERMAID: %s", err)
+	}
+	wanted := `
+graph fsm
+    closed -->|open| open
+    intermediate -->|part-close| closed
+    open -->|close| closed
+`
+	normalizedGot := strings.Replace(got, "\n", "", -1)
+	normalizedWanted := strings.ReplaceAll(wanted, "\n", "")
+	if normalizedGot != normalizedWanted {
+		t.Errorf("build mermaid graph failed. \nwanted \n%s\nand got \n%s\n", wanted, got)
+		fmt.Println([]byte(normalizedGot))
+		fmt.Println([]byte(normalizedWanted))
+	}
+}
