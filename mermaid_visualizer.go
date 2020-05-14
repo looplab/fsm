f6928b4a12ef0da0d67645aaeef52f9561bc8342
Julian Schmidt <julian.schmidt@andrena.de>
2020-05-14 08:58:40 +0200

    refactor: split visualizer into different files

---
+++ mermaid_visualizer.go
@@ -0,0 +1,89 @@
+package fsm
+
+import (
+	"bytes"
+	"fmt"
+)
+
+const highlightingColor = "#00AA00"
+
+// MermaidDiagramType the type of the mermaid diagram type
+type MermaidDiagramType string
+
+const (
+	// FlowChart the diagram type for output in flowchart style (https://mermaid-js.github.io/mermaid/#/flowchart) (including current state)
+	FlowChart MermaidDiagramType = "flowChart"
+	// StateDiagram the diagram type for output in stateDiagram style (https://mermaid-js.github.io/mermaid/#/stateDiagram)
+	StateDiagram MermaidDiagramType = "stateDiagram"
+)
+
+// VisualizeForMermaidWithGraphType outputs a visualization of a FSM in Mermaid format as specified by the graphType.
+func VisualizeForMermaidWithGraphType(fsm *FSM, graphType MermaidDiagramType) (string, error) {
+	switch graphType {
+	case FlowChart:
+		return visualizeForMermaidAsFlowChart(fsm), nil
+	case StateDiagram:
+		return visualizeForMermaidAsStateDiagram(fsm), nil
+	default:
+		return "", fmt.Errorf("unknown MermaidDiagramType: %s", graphType)
+	}
+}
+
+func visualizeForMermaidAsStateDiagram(fsm *FSM) string {
+	var buf bytes.Buffer
+
+	sortedTransitionKeys := getSortedTransitionKeys(fsm.transitions)
+
+	buf.WriteString("stateDiagram\n")
+	buf.WriteString(fmt.Sprintln(`    [*] -->`, fsm.current))
+
+	for _, k := range sortedTransitionKeys {
+		v := fsm.transitions[k]
+		buf.WriteString(fmt.Sprintf(`    %s --> %s: %s`, k.src, v, k.event))
+		buf.WriteString("\n")
+	}
+
+	return buf.String()
+}
+
+// visualizeForMermaidAsFlowChart outputs a visualization of a FSM in Mermaid format (including highlighting of current state).
+func visualizeForMermaidAsFlowChart(fsm *FSM) string {
+	var buf bytes.Buffer
+
+	sortedTransitionKeys := getSortedTransitionKeys(fsm.transitions)
+	sortedStates, statesToIDMap := getSortedStates(fsm.transitions)
+
+	writeFlowChartGraphType(&buf)
+	writeFlowChartStates(&buf, sortedStates, statesToIDMap)
+	writeFlowChartTransitions(&buf, fsm.transitions, sortedTransitionKeys, statesToIDMap)
+	writeFlowChartHightlightCurrent(&buf, fsm.current, statesToIDMap)
+
+	return buf.String()
+}
+
+func writeFlowChartGraphType(buf *bytes.Buffer) {
+	buf.WriteString("graph LR\n")
+}
+
+func writeFlowChartStates(buf *bytes.Buffer, sortedStates []string, statesToIDMap map[string]string) {
+	for _, state := range sortedStates {
+		buf.WriteString(fmt.Sprintf(`    %s[%s]`, statesToIDMap[state], state))
+		buf.WriteString("\n")
+	}
+
+	buf.WriteString("\n")
+}
+
+func writeFlowChartTransitions(buf *bytes.Buffer, transitions map[eKey]string, sortedTransitionKeys []eKey, statesToIDMap map[string]string) {
+	for _, transition := range sortedTransitionKeys {
+		target := transitions[transition]
+		buf.WriteString(fmt.Sprintf(`    %s --> |%s| %s`, statesToIDMap[transition.src], transition.event, statesToIDMap[target]))
+		buf.WriteString("\n")
+	}
+	buf.WriteString("\n")
+}
+
+func writeFlowChartHightlightCurrent(buf *bytes.Buffer, current string, statesToIDMap map[string]string) {
+	buf.WriteString(fmt.Sprintf(`    style %s fill:%s`, statesToIDMap[current], highlightingColor))
+	buf.WriteString("\n")
+}
--- mermaid_visualizer_test.go
+++ mermaid_visualizer_test.go
@@ -6,37 +6,6 @@ import (
 	"testing"
 )
 
-func TestGraphvizOutput(t *testing.T) {
-	fsmUnderTest := NewFSM(
-		"closed",
-		Events{
-			{Name: "open", Src: []string{"closed"}, Dst: "open"},
-			{Name: "close", Src: []string{"open"}, Dst: "closed"},
-			{Name: "part-close", Src: []string{"intermediate"}, Dst: "closed"},
-		},
-		Callbacks{},
-	)
-
-	got := Visualize(fsmUnderTest)
-	wanted := `
-digraph fsm {
-    "closed" -> "open" [ label = "open" ];
-    "intermediate" -> "closed" [ label = "part-close" ];
-    "open" -> "closed" [ label = "close" ];
-
-    "closed";
-    "intermediate";
-    "open";
-}`
-	normalizedGot := strings.ReplaceAll(got, "\n", "")
-	normalizedWanted := strings.ReplaceAll(wanted, "\n", "")
-	if normalizedGot != normalizedWanted {
-		t.Errorf("build graphivz graph failed. \nwanted \n%s\nand got \n%s\n", wanted, got)
-		fmt.Println([]byte(normalizedGot))
-		fmt.Println([]byte(normalizedWanted))
-	}
-}
-
 func TestMermaidOutput(t *testing.T) {
 	fsmUnderTest := NewFSM(
 		"closed",
@@ -48,7 +17,7 @@ func TestMermaidOutput(t *testing.T) {
 		Callbacks{},
 	)
 
-	got, err := VisualizeWithType(fsmUnderTest, MERMAID)
+	got, err := VisualizeForMermaidWithGraphType(fsmUnderTest, StateDiagram)
 	if err != nil {
 		t.Errorf("got error for visualizing with type MERMAID: %s", err)
 	}
@@ -67,7 +36,6 @@ stateDiagram
 		fmt.Println([]byte(normalizedWanted))
 	}
 }
-
 
 func TestMermaidFlowChartOutput(t *testing.T) {
 	fsmUnderTest := NewFSM(
